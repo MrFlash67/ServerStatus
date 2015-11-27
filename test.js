42d0d7622bdf2ac80923981dfbad8a3db03dcc06
MrFlash67 <mrflash67@noreply.github.com>
2015-11-27 14:28:13 +1300

    Some test stuff.

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
 /**
  * Created by MrFlash67 on 27/11/15.
  */
+var http = require('http');
+var url = require('url');
+
+console.log('hey');
+var options = {method: 'HEAD', host: 'muute.net', port: 80, path: '/'};
+
+var req = http.request(options, function(res) {
+	console.log('yo');
+	console.log(res.statusCode);
+	console.log(JSON.stringify(res.headers));
+});
+req.end();
+
+//var  = http.createServer(function(req, res) {
+//	console.log("server running");
+//
+//});
+//server.listen(8081);
